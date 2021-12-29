800fb56e6a3c16b107370a1a2a2266640fc62228
hitoshi <61585229+hitoshidevx@users.noreply.github.com>
2021-12-29 10:56:02 -0300

    Repositorio Iniciado e alguns fundamentos incrementados

---
+++ fundamentos/tipagemFraca.js
@@ -0,0 +1,12 @@
+let seila = 'Sei la'
+console.log(seila)
+console.log(typeof seila)
+
+seila = 3.141592
+console.log(seila)
+console.log(typeof seila)
+
+// Evitar nomes genéricos
+let valor = 12
+let nome = 'gabriel'
+let pqp = false 
---
+++ fundamentos/variaveisEConstantes.js
@@ -0,0 +1,17 @@
+/// Quando for definir uma variável, utilizar sde preferência o 'let'.
+
+var a = 3
+let b = 4
+
+var a = 5
+b = 6 
+
+console.log(a, b)
+
+a = 900
+b = 300
+
+console.log(a, b)
+
+const c = 5
+console.log(c)
